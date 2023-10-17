d65211cd2681732760bfc61ef5096b6e34d353fb
Rudolf Klikh <XVPR43@motorolasolutions.com>
2023-10-17 16:05:08 +0200

    [PoC] [Job Search] - Config Settings + FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from './environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    firebase: {
+            apiKey: "AIzaSyAHz6dqFlNGtGrh8MepkRaLKUx5TagTX00",
+            authDomain: "jobsearch-portfolioapp.firebaseapp.com",
+            projectId: "jobsearch-portfolioapp",
+            storageBucket: "jobsearch-portfolioapp.appspot.com",
+            messagingSenderId: "170919612064",
+    }
+}
